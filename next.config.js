a8cbdc0e216ee224a1cd18307b0a668933afc5dd
CarvalhoDaStreet <carlosccarva@gmail.com>
2022-09-24 18:01:19 +0100

    basic funct

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports ={
+  nextConfig,
+  env:{
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN
+  },
+  images:{
+    domains:['cdn.shopify.com']
+  }
+} 
