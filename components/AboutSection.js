408b980e78088d0e368a2e7b7887025a48fb2408
BENAISSA Abdessamad <abdessamadbenaissa@gmail.com>
2023-02-16 18:06:21 +0100

    fixes

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -7,7 +7,7 @@ const AboutSection = () => {
       <section className="bg-cgrey dark:bg-gray-900">
          <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div className="font-normal  text-black sm:text-lg dark:text-gray-400">
-               <h2 className="mb-4 text-5xl tracking-tight font-eb font-bold md:text-black text-black ">We didn&apost reinvent the wheel</h2>
+               <h2 className="mb-4 text-5xl tracking-tight font-eb font-bold md:text-black text-black ">We didn't reinvent the wheel</h2>
                <p className="mb-4 font-alt ">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
                <p className='font-alt ' >We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
             </div>
